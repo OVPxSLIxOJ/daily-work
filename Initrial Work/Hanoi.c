b9b0aae53edef31b44b02cce3774c979e5f59605
OVPxSLIxOJ <OVPxSLIxOJ@outlook.com>
2025-12-18 17:48:01 +0800

    汉诺塔（文字可视化）

---
+++ Initrial Work/Hanoi.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+void hanoi(int n, char from, char to, char itme)
+{
+	if (n == 1)
+	{
+		printf("move plate %d from %c to %c\n", n, from, to);
+		return;
+	}
+	hanoi(n - 1, from, itme, to);
+	printf("move plate %d from %c to %c", n, from, to);
+	hanoi(n - 1, itme, to, from);
+}
+int main()
+{
+	int n = 0;
+	printf("please enter the number of disks for the tower of hanoi : ");
+	scanf("%d", &n);
+	hanoi(n, 'A', 'B', 'C');
+	return 0;
+}
