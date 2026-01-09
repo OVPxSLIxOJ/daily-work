3f204138b9dac7f66a52add506ba7afbc4accb4b
OVPxSLIxOJ <OVPxSLIxOJ@outlook.com>
2026-01-09 18:00:15 +0800

    指针数组（数据+1）

---
+++ Initrial Work/PointerArray1+1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void add_one(int* arr, int n)
+{
+	int* p;
+	for (p = arr; p < arr + n; p++)
+	{
+		*p += 1;
+	}
+}
+int main()
+{
+	int a[] = { 1, 2, 3 ,4 ,5 };
+	int n = sizeof(a) / sizeof(a[0]);
+	add_one(a, n);
+	int i = 0;
+	for (i = 0; i < n; i++)
+	{
+		printf("%d ", a[i]);
+	}
+	return 0;
+}
